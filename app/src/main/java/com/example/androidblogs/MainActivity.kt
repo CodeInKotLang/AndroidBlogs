3e608300d4219af32974610ad9bc1e87b2615176
Mohammad Arif <codeinkotlang@gmail.com>
2025-01-26 14:56:56 +0530

    designed BlogListScreen

--- app/src/main/java/com/example/androidblogs/MainActivity.kt
+++ app/src/main/java/com/example/androidblogs/MainActivity.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.androidblogs.ui.theme.AndroidBlogsTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.androidblogs.domain.model.Blog
+import com.example.androidblogs.presentation.blog_list.BlogListScreen
+import com.example.androidblogs.presentation.blog_list.BlogListState
+import com.example.androidblogs.presentation.theme.AndroidBlogsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +19,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBlogsTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//
-//                }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    val dummyList = listOf(
+                        Blog(
+                            id = 1,
+                            title = "State Management in Compose",
+                            thumbnailUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQyCrAWdIb8-moiYuP7EdpznRLOLaKoZWJ04MLzMi1wkJrMfLKQshwXhB_ODNz3T6_aoOwQ0YccVpSbLO2K9qkpx-HTklvNm3ZR_spOINLr861_PgDXDnh6LgpptIyzR5Nv-UjlQ-5FyeLpHwOCb4NjZ8darLIomTVjHM2VvDv7YZdzO-FS6zMKEhlCQ/s1600/Android-JetpackCompose1.2-Social.png",
+                            contentUrl = "",
+                            content = null
+                        )
+                    )
+                    BlogListScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        state = BlogListState(blogs = dummyList)
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/androidblogs/presentation/blog_list/BlogListScreen.kt
@@ -0,0 +1,72 @@
+package com.example.androidblogs.presentation.blog_list
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import com.example.androidblogs.domain.model.Blog
+import com.example.androidblogs.presentation.blog_list.component.BlogCard
+
+@Composable
+fun BlogListScreen(
+    modifier: Modifier = Modifier,
+    state: BlogListState
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        BlogListTopBar()
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 300.dp),
+            contentPadding = PaddingValues(15.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            items(state.blogs) { blog ->
+                BlogCard(
+                    blog = blog
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BlogListTopBar(
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        windowInsets = WindowInsets(0),
+        modifier = modifier,
+        title = { Text(text = "Android Blogs") }
+    )
+}
+
+
+@PreviewScreenSizes
+@Composable
+private fun PreviewBlogListScreen() {
+    val dummyList = listOf(
+        Blog(
+            id = 1,
+            title = "State Management in Compose",
+            thumbnailUrl = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQyCrAWdIb8-moiYuP7EdpznRLOLaKoZWJ04MLzMi1wkJrMfLKQshwXhB_ODNz3T6_aoOwQ0YccVpSbLO2K9qkpx-HTklvNm3ZR_spOINLr861_PgDXDnh6LgpptIyzR5Nv-UjlQ-5FyeLpHwOCb4NjZ8darLIomTVjHM2VvDv7YZdzO-FS6zMKEhlCQ/s1600/Android-JetpackCompose1.2-Social.png",
+            contentUrl = "",
+            content = null
+        )
+    )
+    BlogListScreen(state = BlogListState(blogs = dummyList))
+}
