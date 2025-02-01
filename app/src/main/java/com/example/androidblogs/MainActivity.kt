84ca1cf6b670ddbc2b3cf5215e25a0fe197dacc6
Mohammad Arif <codeinkotlang@gmail.com>
2025-02-01 14:23:26 +0530

    splash screen and app icon added

--- app/src/main/java/com/example/androidblogs/MainActivity.kt
+++ app/src/main/java/com/example/androidblogs/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.androidblogs.presentation.navigation.NavGraph
 import com.example.androidblogs.presentation.theme.AndroidBlogsTheme
@@ -15,6 +16,7 @@ import com.example.androidblogs.presentation.theme.AndroidBlogsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AndroidBlogsTheme {
